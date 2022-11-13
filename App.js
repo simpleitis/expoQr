6e4b200ba22859ed6bd5d5c9fa5c90ab8298ecf6
Amar <amarkrishna25@gmail.com>
2022-11-13 21:01:21 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [scanData, setScanData] = React.useState();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text>Please grant camera permissions to app.</Text>
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanData(data);
+    alert(`Data: ${data}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BarCodeScanner
+        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+        style={StyleSheet.absoluteFillObject}
+        onBarCodeScanned={scanData ? undefined : handleBarCodeScanned}
+      />
+      {scanData && (
+        <Button
+          title="Scan Again?"
+          onPress={() => setScanData(undefined)}
+          color="#007cff"
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
